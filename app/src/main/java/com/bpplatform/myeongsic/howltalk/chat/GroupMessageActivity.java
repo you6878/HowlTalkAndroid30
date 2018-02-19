716cfccb169a5697e61af0593028491522d3c000
유명식 <myeongsic@MacBook-Pro.local>
2018-02-19 23:20:56 +0900

    init

---
+++ app/src/main/java/com/bpplatform/myeongsic/howltalk/chat/GroupMessageActivity.java
@@ -0,0 +1,165 @@
+package com.bpplatform.myeongsic.howltalk.chat;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.bpplatform.myeongsic.howltalk.R;
+import com.bpplatform.myeongsic.howltalk.model.ChatModel;
+import com.bpplatform.myeongsic.howltalk.model.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupMessageActivity extends AppCompatActivity {
+    Map<String,UserModel> users = new HashMap<>();
+    String destinationRoom;
+    String uid;
+    EditText editText;
+
+    private UserModel destinationUserModel;
+    private DatabaseReference databaseReference;
+    private ValueEventListener valueEventListener;
+
+    private RecyclerView recyclerView;
+
+    List<ChatModel.Comment> comments = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_message);
+        destinationRoom = getIntent().getStringExtra("destinationRoom");
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        editText = (EditText)findViewById(R.id.groupMessageActivity_editText);
+        FirebaseDatabase.getInstance().getReference().child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                users = (Map<String, UserModel>) dataSnapshot.getValue();
+                init();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        recyclerView = (RecyclerView) findViewById(R.id.groupMessageActivity_recyclerview);
+        recyclerView.setAdapter(new GroupMessageRecyclerViewAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+    void init(){
+        Button button = (Button) findViewById(R.id.groupMessageActivity_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChatModel.Comment comment = new ChatModel.Comment();
+                comment.uid = uid;
+                comment.message = editText.getText().toString();
+                comment.timestamp = ServerValue.TIMESTAMP;
+                FirebaseDatabase.getInstance().getReference().child("chatrooms").child(destinationRoom).child("comments").push().setValue(comment).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        editText.setText("");
+                    }
+                });
+
+            }
+        });
+    }
+    class GroupMessageRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+        public GroupMessageRecyclerViewAdapter(){
+
+            getMessageList();
+        }
+        void getMessageList() {
+            databaseReference = FirebaseDatabase.getInstance().getReference().child("chatrooms").child(destinationRoom).child("comments");
+            valueEventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    comments.clear();
+                    Map<String, Object> readUsersMap = new HashMap<>();
+                    for (DataSnapshot item : dataSnapshot.getChildren()) {
+                        String key = item.getKey();
+                        ChatModel.Comment comment_origin = item.getValue(ChatModel.Comment.class);
+                        ChatModel.Comment comment_motify = item.getValue(ChatModel.Comment.class);
+                        comment_motify.readUsers.put(uid, true);
+
+                        readUsersMap.put(key, comment_motify);
+                        comments.add(comment_origin);
+                    }
+
+                    if (!comments.get(comments.size() - 1).readUsers.containsKey(uid)) {
+
+
+                        FirebaseDatabase.getInstance().getReference().child("chatrooms").child(destinationRoom).child("comments")
+                                .updateChildren(readUsersMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                notifyDataSetChanged();
+                                recyclerView.scrollToPosition(comments.size() - 1);
+                            }
+                        });
+                    } else {
+                        notifyDataSetChanged();
+                        recyclerView.scrollToPosition(comments.size() - 1);
+                    }
+                    //메세지가 갱신
+
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message,parent,false);
+
+
+            return new GroupMessageViewHodler(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return comments.size();
+        }
+
+        private class GroupMessageViewHodler extends RecyclerView.ViewHolder {
+            public GroupMessageViewHodler(View view) {
+                super(view);
+            }
+        }
+    }
+}
